f3a333a64f9b15eab2e2f44bc3e02990eefe5128
Jordan Smith <jordansmith@Jordans-MacBook-Air-2.local>
2021-11-29 13:53:54 +0000

    sidebar,routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import './App.css';
 import ArtistContainer from './containers/ArtistContainer';
-import Navbar from './components/navbar/Navbar';
-
+import Sidebar from './components/sidebar/Sidebar';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
 
 
 function App() {
   return (
-    <div>
-      <Navbar/>
-      <ArtistContainer/>
-    </div>
+    // <div>
+    //   <Navbar/>
+    //   <ArtistContainer/>
+    // </div>
+
+    <div className="App">
+    <Router>
+      <Sidebar />
+      <Switch>
+       
+        <Route path='/artists' component={ArtistContainer}/>
+        
+      </Switch>
+    </Router>
+  </div>
   );
 }
 
 export default App;
+
+/* in the return statement: 
+ <div className="App">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={AlbumContainer}/>  
+          <Route path='/artists' component={ArtistContainer}/>
+          <Route path='/songs' component={SongContainer}/>
+          <Route path='/playlist' component={PlayListContainer}/>
+          <Route path='/admin' component={AdminContainer}/>
+        </Switch>
+      </Router>
+    </div>
+
+
+*/
