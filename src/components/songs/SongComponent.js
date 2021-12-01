87708334752393d6b63df3628a058b01f4e9e96f
Darania Muralitharan <90543261+DanaMurali@users.noreply.github.com>
2021-12-01 16:48:46 +0000

    trying to add play button on hover

--- src/components/songs/SongComponent.js
+++ src/components/songs/SongComponent.js
@@ -1,26 +1,37 @@
 import React from 'react'
-import {useState} from 'react'
+import { useState } from 'react'
 import placeholder from '../../placeholder/songplaceholder.jpeg'
-import { BsTrash, BsFillTrashFill, BsHeart, BsHeartFill } from "react-icons/bs";
+import { BsTrash, BsFillTrashFill, BsHeart, BsHeartFill, BsPlayCircle, BsPlayCircleFill } from "react-icons/bs";
+import './Song.css'
 
-
-const SongComponent= ({song}) => {
+const SongComponent = ({ song }) => {
 
     const [modeHeart, setModeHeart] = useState(false);
+    const [modeButton, setModeButton] = useState(false);
 
     const handleOnClick = () => {
-        setModeHeart(!modeHeart);  
-   }
+        setModeHeart(!modeHeart);
+    }
 
-    return(
-        <div className= "component">
+    const handleOnClick2 = () => {
+        setModeButton(!modeButton);
+    }
 
-            <img src = {placeholder} alt={song.name}/>    
-            <h4>Name:</h4>
-            <p>{song.song_name}</p>
-            <h4>Genre:</h4>
-            <p>{song.genre}</p>
-            {/* <h4>Duration:</h4>
+
+    return (
+        <>
+        
+            <div className="component">
+
+                <img src={placeholder} alt={song.name} />
+                <h4>Name:</h4>
+                <p>{song.song_name}</p>
+                <h4>Genre:</h4>
+                <p>{song.genre}</p>
+
+
+
+                {/* <h4>Duration:</h4>
             <p>{song.duration}</p>
             <h4>Artist ID:</h4>
             <p>{song.artist_id}</p>
@@ -32,24 +43,35 @@ const SongComponent= ({song}) => {
             <p>{song.languages}</p>
             <h4>Platform:</h4>
             <p>{song.platform}</p> */}
-             <div className="heart-container">
-            {/* <div className="float-icon">
+            
+            <div className="PlayButton">
+                {modeButton ?
+                    (<BsPlayCircleFill size={75} color={'lightblue'} onClick={handleOnClick2} />)
+                    :
+                    (<BsPlayCircle size={75} onClick={handleOnClick2} />)
+                }
+            </div>
+            
+
+                <div className="heart-container">
+                    {/* <div className="float-icon">
                         { modeTrash ? 
                             (<BsFillTrashFill onClick={handleOnClick2 }/>)
                             :
                             (<BsTrash onClick={handleOnClick2 }/>)
                         }
             </div> */}
-            <div className="float-icon">
-                        { modeHeart ? 
-                        (<BsHeartFill size={28}  color={'hotpink'} onClick={handleOnClick }/>)
-                        :
-                        (<BsHeart size={28} onClick={handleOnClick }/>)
+                    <div className="float-icon">
+                        {modeHeart ?
+                            (<BsHeartFill size={28} color={'hotpink'} onClick={handleOnClick} />)
+                            :
+                            (<BsHeart size={28} onClick={handleOnClick} />)
                         }
-            </div>
-            </div>
+                    </div>
+                </div>
 
-        </div>
+            </div>
+        </>
     )
 
 }
